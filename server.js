2fe56f342ca39dbc64b11fa3414e1c739b26bdd8
zk779 <59329882+zk779@users.noreply.github.com>
2026-01-12 13:08:31 +0500

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const app = express();
 const prisma = new PrismaClient();
 
 // ✅ CORS must come BEFORE routes
-const allowedOrigins = ["http://localhost:5173", "http://127.0.0.1:5173"];
+const allowedOrigins = ["http://localhost:5173", "https://al-rayyan-travels.vercel.app"];
 
 app.use(
   cors({
