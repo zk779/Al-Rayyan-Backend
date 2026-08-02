3471b8336866ade6fc549a9ba1dd863a5e0e6492
zk779 <59329882+zk779@users.noreply.github.com>
2026-08-02 20:22:34 +0500

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ const prisma = new PrismaClient();
 // ✅ CORS must come BEFORE routes
 const allowedOrigins = [
 	"https://al-rayyan-travels.vercel.app",
-	"https://al-rayyan-backend-eight.vercel.app/",
+	"https://al-rayyan-backend-eight.vercel.app",
 	"http://localhost:5173",
 	"http://127.0.0.1:5173",
 ];
